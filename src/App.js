8ced0ef980b761f2a67f1e2cab9ad0d1e862f11f
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-27 19:17:22 +0500

    change functional component to a class component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import CardList from "./Components/CardList";
 import { food } from "./food";
 import SearchBox from "./Components/SearchBox";
 
-const App = () => {
-  return (
-    <div>
-      <h1 className="site-header">Food Filter</h1>
-      <SearchBox />
-      <CardList food={food} />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="site-header">Food Filter</h1>
+        <SearchBox />
+        <CardList food={food} />
+      </div>
+    );
+  }
+}
 
 export default App;
