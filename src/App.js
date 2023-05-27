8531d5d3c02253473249f5fd30ac043791d866f1
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-27 19:16:14 +0500

    add search box and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardList from "./Components/CardList";
 import { food } from "./food";
+import SearchBox from "./Components/SearchBox";
 
 const App = () => {
   return (
     <div>
       <h1 className="site-header">Food Filter</h1>
+      <SearchBox />
       <CardList food={food} />
     </div>
   );
---
+++ src/Components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <div className="search-box">
+      <input
+        className="search-input"
+        type="search"
+        placeholder="Search some food..."
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
