a5a538a8e71daa4fa2a33471b499aced14174545
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-27 19:11:03 +0500

    change class component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import CardList from "./Components/CardList";
+import { food } from "./food";
 
 const App = () => {
   return (
     <div>
       <h1 className="site-header">Food Filter</h1>
-      <CardList />
+      <CardList food={food} />
     </div>
   );
 };
--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -1,24 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "./Card";
-import { food } from "../food";
 
-class CardList extends Component {
-  render() {
-    return (
-      <div className="card-list">
-        {food.map((food, index) => {
-          return (
-            <Card
-              key={index}
-              img={food.img}
-              name={food.name}
-              price={food.price}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ food }) => {
+  return (
+    <div className="card-list">
+      {food.map((food, index) => {
+        return (
+          <Card
+            key={index}
+            img={food.img}
+            name={food.name}
+            price={food.price}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CardList;
